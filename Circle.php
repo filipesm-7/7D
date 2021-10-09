cd8cf43673adcf1c7b33617d1ea9d3808eca555c
Filipe Mendonça <filipesm.7@gmail.com>
2021-10-09 16:39:50 +0100

    add Circle class

---
+++ Circle.php
@@ -0,0 +1,49 @@
+<?php
+
+
+/**
+ * Class Circle
+ */
+abstract class Circle extends Shape
+{
+    /**
+     * @var int
+     */
+    const Y = 3;
+
+    /**
+     * @var float
+     */
+    protected $radius;
+
+    /**
+     * Circle constructor
+     *
+     * @param float $width
+     * @param float $length
+     * @param float $radius
+     */
+    public function __construct(float $width, float $length, float $radius) {
+        parent::__construct($width, $length);
+
+        $this->radius = $radius;
+    }
+
+    /**
+     * @return float
+     */
+    public function getArea(): float {
+        echo pi() * pow($this->radius, 2);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProps() {
+        return [
+            'name' => $this->name,
+            'radius' => $this->radius,
+            'id' => $this->getId(),
+        ];
+    }
+}
